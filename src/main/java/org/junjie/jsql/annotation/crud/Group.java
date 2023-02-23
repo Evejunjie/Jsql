02f445f8b4901732e5ed721e230984439a0e3c41
Evejunjie <17749922376@163.com>
2023-02-23 18:30:58 +0800

    分组类

---
+++ src/main/java/org/junjie/jsql/annotation/crud/Group.java
@@ -0,0 +1,16 @@
+package org.junjie.jsql.annotation.crud;
+
+/**
+ * 查询条件 如果遇到复杂的,需要使用 该类提供的注解
+ * 
+ * 在子类声明 BetweenValue 则是 Between 查询 
+ * 
+ * 在子类声明 List,Set 则是 IN 查询
+ * 
+ * @author junjie
+ *
+ */
+public abstract class Group {
+
+	
+}
